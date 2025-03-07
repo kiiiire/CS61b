09cda5184923e37e2705ff8bdc8d8b325b54fb1e
kiiiire <3187358920cc@gmail.com>
2025-03-07 20:22:08 +0800

    first

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -2,18 +2,12 @@ package DebugExercise;
 
 /**
  * Exercise to showcase the step over button.
- * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
+ * Code adapted from <a href="https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java">...</a> and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        return Math.max(a, b);
     }
 
 
@@ -46,7 +40,7 @@ public class DebugExercise2 {
         }
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
+            int biggerValue = Math.max(a[i], b[i]);
             returnArray[i] = biggerValue;
         }
 
@@ -58,7 +52,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = sum + x[i];
             i = i + 1;
         }
         return sum;
