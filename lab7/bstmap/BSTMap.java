1cb49ccb38e653a6c16271926be35c54c7239bfa
kiiiire <3187358920cc@gmail.com>
2025-03-30 15:53:27 +0800

    final

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,104 +1,169 @@
 package bstmap;
 
-import java.util.Iterator;
 import java.util.Set;
-import java.util.HashSet;
+import java.util.Iterator;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
-    private class Node {
-        K key;
-        V value;
-        Node left, right;
 
-        Node(K k, V v) {
-            key = k;
-            value = v;
+    private Node root;             // root of BST
+
+    private class Node {
+        private final K key;           // sorted by key
+        private V val;         // associated data
+        private Node left, right;  // left and right subtrees
+        private int size;          // number of nodes in subtree
+
+        public Node(K key, V val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+            this.left = null;
+            this.right = null;
         }
     }
 
-    private Node root;
-    private int size;
-
     public BSTMap() {
         root = null;
-        size = 0;
     }
 
     @Override
     public void clear() {
         root = null;
-        size = 0;
+    }
+
+    private class ReturnValue {
+        private final boolean found;
+        private final Node node;
+
+        public ReturnValue(boolean found, Node node) {
+            this.found = found;
+            this.node = node;
+        }
+
+        public boolean getFound() {
+            return found;
+        }
+
+        public Node getNode() {
+            return node;
+        }
+    }
+
+    private ReturnValue get(Node x, K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with a null key");
+        }
+        if (x == null) {
+            return new ReturnValue(false, null);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return new ReturnValue(true, x);
+        }
     }
 
     @Override
     public boolean containsKey(K key) {
-        if (key == null) {
-            throw new IllegalArgumentException("argument to contains() is null");
-        }
         return get(key) != null;
     }
 
     @Override
     public V get(K key) {
-        return getHelper(root, key);
+        ReturnValue returnValue = get(root, key);
+        if (returnValue.getFound()) {
+            return returnValue.getNode().val;
+        } else {
+            return null;
+        }
     }
 
-    private V getHelper(Node node, K key) {
-        if (node == null) return null;
-        int cmp = key.compareTo(node.key);
-        if (cmp < 0) return getHelper(node.left, key);
-        else if (cmp > 0) return getHelper(node.right, key);
-        else return node.value;
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else return x.size;
     }
 
     @Override
     public int size() {
-        return size;
+        return size(root);
+    }
+
+    private Node put(Node x, K key, V value) {
+        if (x == null) {
+            return new Node(key, value, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, value);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, value);
+        } else {
+            x.val = value;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
     }
 
     @Override
     public void put(K key, V value) {
-        root = putHelper(root, key, value);
-    }
-
-    private Node putHelper(Node node, K key, V value) {
-        if (node == null) {
-            size++;
-            return new Node(key, value);
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with a null key");
         }
-        int cmp = key.compareTo(node.key);
-        if (cmp < 0) node.left = putHelper(node.left, key, value);
-        else if (cmp > 0) node.right = putHelper(node.right, key, value);
-        else node.value = value;
-        return node;
+        root = put(root, key, value);
     }
 
     @Override
     public Set<K> keySet() {
-        Set<K> keySet = new HashSet<>();
-        addKeys(root, keySet);
-        return keySet;
-    }
-
-    private void addKeys(Node node, Set<K> set) {
-        if (node == null) return;
-        set.add(node.key);
-        addKeys(node.left, set);
-        addKeys(node.right, set);
+        return Set.of();
     }
 
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException("remove not implemented");
+        return null;
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException("remove(key, value) not implemented");
+        return null;
+    }
+
+    /* An iterator class for BSTMap. */
+    private class BSTMapIterator implements Iterator<K> {
+        private final Iterator<K> iterator;
+
+        public BSTMapIterator() {
+            Set<K> keySet = keySet();
+            iterator = keySet.iterator();
+        }
+
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        public K next() {
+            return iterator.next();
+        }
     }
 
     @Override
     public Iterator<K> iterator() {
-        return keySet().iterator();
+        return new BSTMapIterator();
+    }
+
+    private void printInOrder(Node x) {
+        if (x == null) {
+            return;
+        }
+        printInOrder(x.left);
+        System.out.println("Key: " + x.key + " | Value: " + x.val);
+        printInOrder(x.right);
+    }
+
+    public void printInOrder() {
+        printInOrder(root);
     }
 }
